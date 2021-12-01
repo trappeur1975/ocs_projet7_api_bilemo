a75e558bbc06e395959df5d5f8b316bc09f1915b
Tr@ppeur1975 <trappeur1975photmail.com>
2021-12-01 20:35:50 +0400

    Creation of fixtures ("CompagnyFixtures" and "ProductFixtures"), of the ApiController controller (for test api via postman)

--- README.md
+++ README.md
@@ -1,18 +1,33 @@
 # ocsProjet7_Api-bilemo
 
-
 ----------------------infos doc--------------------
+
 versions symfony : https://symfony.com/releases
 bundles symfony :
     https://packagist.org/
     https://flex.symfony.com/
 
 ----------------------infos commande--------------------
+
 penser a executer wampserver pour acceder a la base de donnee via phpmyadmin
 
 commande pour excuter le serveur web de symfony :
     symfony server:start
     Dans son navigateur aller a l adresse : http://127.0.0.1:8000
+
+commande pour excuter les fixtures :
+    cette commande vide totalement la base de données avant d'insérer les nouvelles données:
+        php bin/console doctrine:fixtures:load
+
+dans le fichier "composer.json" un script (que j ai nommé « reset-data ») a été crée pour remettre a zero ma base de donnée, surtout utile pour l'utilisation d'un jeu de donnée via des fixtures. Pour l executer il suffit d'executer la commande suivante :
+            
+            composer reset-data
+    
+    apres il faudra creer un dossier "migrations" a la racine du projet si il n'existe pas
+    puis lancer les commandes suivantes :
+        php bin/console doctrine:migrations:generate
+        php bin/console make:migration
+        php bin/console doctrine:migrations:migrate
 
 ----------------------infos project--------------------
 
---
+++ src/Controller/ApiController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Product;
+use App\Repository\ProductRepository;
+use App\Repository\CompagnyRepository;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+/**
+ * @Route("/api", name="api")
+ */
+class ApiController extends AbstractController
+{
+    /**
+     * @Route("/", name="apiIndex")
+     */
+    public function index(): Response
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/ApiController.php',
+        ]);
+    }
+
+    /**
+     * @Route("/compagnies", name="compagny_list", methods={"GET"})
+     */
+    public function listCompagny(CompagnyRepository $repo, SerializerInterface $serializer)
+    {
+        $compagnies = $repo->findAll();
+
+        $data = $serializer->serialize($compagnies, 'json');
+
+        $response = new Response($data);
+        $response->headers->set('Content-Type', 'application/json');
+
+        return $response;
+    }
+
+    /**
+     * @Route("/products", name="product_list", methods={"GET"})
+     */
+    public function listProduct(ProductRepository $repo, SerializerInterface $serializer)
+    {
+        $products = $repo->findAll();
+
+        $data = $serializer->serialize($products, 'json');
+
+        $response = new Response($data);
+        $response->headers->set('Content-Type', 'application/json');
+
+        return $response;
+    }
+
+    /**
+     * @Route("/product/{id}", name="product_show", methods={"GET"})
+     */
+    public function showAction(Product $product, SerializerInterface $serializer)
+    {
+        $data = $serializer->serialize($product, 'json');
+        // $data = $serializer->serialize($article, 'json', ['groups' => 'list']);
+
+        $response = new Response($data);
+        $response->headers->set('Content-Type', 'application/json');
+
+        return $response;
+    }
+}
